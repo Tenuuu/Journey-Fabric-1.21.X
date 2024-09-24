b3944aab32f8ee86c69519d6cb26a080ed204561
Charles Tenuta <ctenuta@uiowa.edu>
2024-09-24 15:30:44 -0500

    finish blocks

--- src/main/java/net/tenu/journey/Journey.java
+++ src/main/java/net/tenu/journey/Journey.java
@@ -2,6 +2,7 @@ package net.tenu.journey;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tenu.journey.block.ModBlocks;
 import net.tenu.journey.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class Journey implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
