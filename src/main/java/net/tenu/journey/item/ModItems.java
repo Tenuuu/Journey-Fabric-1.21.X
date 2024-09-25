a1a7232d99074fd92658586913212dabc7801c6a
Tenuuu <charlestenuta@gmail.com>
2024-09-24 23:08:06 -0500

    added accessories dependencies

--- src/main/java/net/tenu/journey/item/ModItems.java
+++ src/main/java/net/tenu/journey/item/ModItems.java
@@ -3,14 +3,29 @@ package net.tenu.journey.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.tenu.journey.Journey;
 
+import java.util.List;
+
 public class ModItems {
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
-    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+//    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+//    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item RING_OF_FORTITUDE = registerItem("ring_of_fortitude", new Item(new Item.Settings().maxCount(1))
+//    {
+//        @Override
+//        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type)
+//        {
+//            tooltip.add(Text.translatable("tooltip.journey.ring_of_fortitude.tooltip"));
+//            super.appendTooltip(stack, context, tooltip, type);
+//        }
+//    }
+    );
 
 
     private static Item registerItem(String name, Item item) {
@@ -21,8 +36,12 @@ public class ModItems {
         Journey.LOGGER.info("Registering mod items for " + Journey.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(PINK_GARNET);
-            entries.add(RAW_PINK_GARNET);
+//            entries.add(PINK_GARNET);
+//            entries.add(RAW_PINK_GARNET);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(RING_OF_FORTITUDE);
         });
     }
 }
