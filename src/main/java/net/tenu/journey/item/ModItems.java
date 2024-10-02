1e4d5909de84f31869da9b9c344e339a012159f4
Charles Tenuta <ctenuta@uiowa.edu>
2024-10-02 12:45:36 -0500

    changed from accessories to trinkets among many other changes

--- src/main/java/net/tenu/journey/item/ModItems.java
+++ src/main/java/net/tenu/journey/item/ModItems.java
@@ -14,14 +14,32 @@ import net.tenu.journey.Journey;
 import java.util.List;
 
 public class ModItems {
-//    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
-//    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
-    public static final Item RING_OF_FORTITUDE = registerItem("ring_of_fortitude", new Item(new Item.Settings().maxCount(1)));
-    public static final Item RING_OF_FORTITUDE_1 = registerItem("ring_of_fortitude_1", new Item(new Item.Settings().maxCount(1)));
-    public static final Item RING_OF_FORTITUDE_2 = registerItem("ring_of_fortitude_2", new Item(new Item.Settings().maxCount(1)));
-    public static final Item RING_OF_FORTITUDE_3 = registerItem("ring_of_fortitude_3", new Item(new Item.Settings().maxCount(1)));
+    // TRINKETS (DEPRECATED)
+    // public static final Item RING_OF_FORTITUDE = registerItem("ring_of_fortitude", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_FORTITUDE_1 = registerItem("ring_of_fortitude_1", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_FORTITUDE_2 = registerItem("ring_of_fortitude_2", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_FORTITUDE_3 = registerItem("ring_of_fortitude_3", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_KNOWLEDGE = registerItem("ring_of_knowledge", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_KNOWLEDGE_1 = registerItem("ring_of_knowledge_1", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_KNOWLEDGE_2 = registerItem("ring_of_knowledge_2", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_KNOWLEDGE_3 = registerItem("ring_of_knowledge_3", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item GOLDEN_BRACER = registerItem("golden_bracer", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item RING_OF_GREED = registerItem("ring_of_greed", new Item(new Item.Settings().maxCount(1)));
+    // public static final Item SYNERGY = registerItem("synergy", new Item(new Item.Settings().maxCount(1)));
 
-    public static final Item GOLDEN_BRACER = registerItem("golden_bracer", new Item(new Item.Settings().maxCount(1)));
+    // GADGETS
+    public static final Item LIGHT_FEATHER = registerItem("light_feather", new Item(new Item.Settings().maxCount(1)));
+    public static final Item WIND_JUMP = registerItem("wind_jump", new Item(new Item.Settings().maxCount(1)));
+    public static final Item TOTEM_OF_REGENERATION = registerItem("totem_of_regeneration", new Item(new Item.Settings().maxCount(1)));
+    public static final Item GRAPPLING_HOOK = registerItem("grappling_hook", new Item(new Item.Settings().maxCount(1)));
+    public static final Item GONG_OF_WEAKENING = registerItem("gong_of_weakening", new Item(new Item.Settings().maxCount(1)));
+    public static final Item BOOTS_OF_SWIFTNESS = registerItem("boots_of_swiftness", new Item(new Item.Settings().maxCount(1)));
+    public static final Item TOTEM_OF_SHIELDING = registerItem("totem_of_shielding", new Item(new Item.Settings().maxCount(1)));
+    public static final Item WIND_HORN = registerItem("wind_horn", new Item(new Item.Settings().maxCount(1)));
+    public static final Item DEATH_CAP_MUSHROOM = registerItem("death_cap_mushroom", new Item(new Item.Settings().maxCount(1)));
+    public static final Item GHOST_CLOAK = registerItem("ghost_cloak", new Item(new Item.Settings().maxCount(1)));
+
+    // ITEMS
     public static final Item FOUR_LEAF_CLOVER = registerItem("four_leaf_clover", new Item(new Item.Settings()));
 //    {
 //        @Override
@@ -31,6 +49,8 @@ public class ModItems {
 //            super.appendTooltip(stack, context, tooltip, type);
 //        }
 //    }
+//    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+//    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
 
     private static Item registerItem(String name, Item item) {
@@ -47,12 +67,30 @@ public class ModItems {
         });
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
-            entries.add(RING_OF_FORTITUDE);
-            entries.add(RING_OF_FORTITUDE_1);
-            entries.add(RING_OF_FORTITUDE_2);
-            entries.add(RING_OF_FORTITUDE_3);
+            // TRINKETS (DEPRECATED)
+            // entries.add(RING_OF_FORTITUDE);
+            // entries.add(RING_OF_FORTITUDE_1);
+            // entries.add(RING_OF_FORTITUDE_2);
+            // entries.add(RING_OF_FORTITUDE_3);
+            // entries.add(GOLDEN_BRACER);
+            // entries.add(RING_OF_GREED);
+            // entries.add(RING_OF_KNOWLEDGE);
+            // entries.add(RING_OF_KNOWLEDGE_1);
+            // entries.add(RING_OF_KNOWLEDGE_2);
+            // entries.add(RING_OF_KNOWLEDGE_3);
+            // entries.add(SYNERGY);
 
-            entries.add(GOLDEN_BRACER);
+            // GADGETS
+            entries.add(LIGHT_FEATHER);
+            entries.add(WIND_JUMP);
+            entries.add(TOTEM_OF_REGENERATION);
+            entries.add(GRAPPLING_HOOK);
+            entries.add(GONG_OF_WEAKENING);
+            entries.add(BOOTS_OF_SWIFTNESS);
+            entries.add(TOTEM_OF_SHIELDING);
+            entries.add(WIND_HORN);
+            entries.add(DEATH_CAP_MUSHROOM);
+            entries.add(GHOST_CLOAK);
         });
     }
 }
