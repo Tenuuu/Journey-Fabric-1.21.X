bec750cc4e3aaf1dca23f28a57249060ae765c6e
Tenuuu <charlestenuta@gmail.com>
2024-10-16 11:30:35 -0500

    added MeleeSpeedAuraEffect

--- src/main/java/net/tenu/journey/effect/ModEffects.java
+++ src/main/java/net/tenu/journey/effect/ModEffects.java
@@ -8,9 +8,11 @@ import net.minecraft.util.Identifier;
 public class ModEffects {
     public static final StatusEffect TOTEM_REGEN = new TotemRegenEffect();
     public static final StatusEffect DAMAGE_AURA = new DamageAuraEffect();
+    public static final StatusEffect MELEE_SPEED_AURA = new MeleeSpeedAuraEffect();
 
     public static void registerModEffects() {
         Registry.register(Registries.STATUS_EFFECT, Identifier.of("journey", "totem_regen"), TOTEM_REGEN);
         Registry.register(Registries.STATUS_EFFECT, Identifier.of("journey", "damage_aura"), DAMAGE_AURA);
+        Registry.register(Registries.STATUS_EFFECT, Identifier.of("journey", "melee_speed_aura"), MELEE_SPEED_AURA);
     }
 }
